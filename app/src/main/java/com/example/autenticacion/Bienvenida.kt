f1b7ee7c1307d4ea874f03aa722661ee9d06c809
Usuario <saragl728@gmail.com>
2024-01-08 10:19:44 +0100

    Añado FirebaseFirestore y hago que se pueda registrar coches

--- app/src/main/java/com/example/autenticacion/Bienvenida.kt
+++ app/src/main/java/com/example/autenticacion/Bienvenida.kt
@@ -3,8 +3,10 @@ package com.example.autenticacion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.autenticacion.databinding.ActivityBienvenidaBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Bienvenida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +16,41 @@ class Bienvenida : AppCompatActivity() {
         title = "Bienvenida"
         val extras = intent.extras
 
+        val db = FirebaseFirestore.getInstance()    //necesario para trabajar con la base de datos
+
         //se le pasa el nombre de usuario que se puso en la actividad anterior
         val nombre = extras?.getString("nombreUsuario")
 
         //el saludo incluirá el nombre de usuario
         binding.saludo.text = "Bienvenido/a, $nombre"
+
+        //guardar coche
+        binding.bAnyadirCoche.setOnClickListener {
+            //se comprueba que no hay campos en blanco
+            if (binding.marca.text.isNotEmpty() && binding.modelo.text.isNotEmpty() && binding.matricula.text.isNotEmpty() && binding.color.text.isNotEmpty()) {
+                db.collection("coches").add(
+                    mapOf(
+                        "color" to binding.color.text,
+                        "marca" to binding.marca.text,
+                        "matricula" to binding.matricula.text,
+                        "modelo" to binding.modelo.text
+                    )
+                )
+                    //se añade un mensaje si ha funcionado
+                    .addOnSuccessListener { documento ->
+                        Toast.makeText(this, "Nuevo coche añadido con el id: ${documento.id}", Toast.LENGTH_LONG).show()
+                    }
+                    //se añade un mensaje si ha habido un error
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Error añadiendo el coche", Toast.LENGTH_LONG).show()
+                    }
+
+            } else {
+                //si un campo está vacío, muestra un toast
+                Toast.makeText(this, "Algún campo está vacío", Toast.LENGTH_LONG).show()
+            }
+        }
+
 
         //cierre de sesión
         binding.cierreSesion.setOnClickListener {
